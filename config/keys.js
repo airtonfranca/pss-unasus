68efc57634b093d64821687dfbb397c60d1fd81f
Airton França <airtonfrance@gmail.com>
2021-07-02 14:18:04 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://airtonfranca:zTfCWuN7fMDC1iSq@pssunasus-shard-00-00.behun.mongodb.net:27017,pssunasus-shard-00-01.behun.mongodb.net:27017,pssunasus-shard-00-02.behun.mongodb.net:27017/Users?ssl=true&replicaSet=atlas-xw7653-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "",
   secretOrKey: "secret",
 };
